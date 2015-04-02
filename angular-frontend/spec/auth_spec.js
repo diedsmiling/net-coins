cd51507e0ddc1101eb6e301a92b0f43b98c949c0
lazarev <diedsmiling@gmail.com>
2015-04-02 22:05:14 +0300

    Added logout function unit test

--- angular-frontend/spec/auth_spec.js
+++ angular-frontend/spec/auth_spec.js
@@ -8,6 +8,16 @@ describe('loginController', function() {
     var $controller, scope, ctrl, Auth, resp,
         tokenResponse = function(){
             return {"token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJzdWIiOiIxIiwiaXNzIjoiaHR0cDpcL1wvbG9jYWxob3N0OjgwMDBcL2FkbWluXC9hdXRoIiwiaWF0IjoiMTQyNzgzMzc4NyIsImV4cCI6IjE0Mjc4MzczODciLCJuYmYiOiIxNDI3ODMzNzg3IiwianRpIjoiODc1NDgwZTZiZTI5ZTg0OWE4NmUyMGVhNmQ5MzliZDYifQ.YWJjYjA2NDJjMGVjM2M0ZjZlOGMxMTNlMGVlYzMxNzBmMzg1MWFlZjI3ZDc5OWEwYTcyMTgyZTdjNzAxOTk1YQ'};
+        },
+        tokenClaims = function(){
+            return {
+              exp: '1427837387',
+              iat: '1427833787',
+              iss: 'http://localhost:8000/admin/auth',
+              jti: '875480e6be29e849a86e20ea6d939bd6',
+              nbf: '1427833787',
+              sub: '1'
+            }
         };
 
     beforeEach(inject(function($localStorage, _Auth_, _$httpBackend_, $controller, $rootScope){
@@ -18,7 +28,7 @@ describe('loginController', function() {
 
     }));
 
-    it('login function', function(){
+    it('Login function', function(){
         delete scope.token;
         expect(scope.token).not.toBeDefined();
         scope.data = {email: 'diedsmiling@gmail.com', password: '1234'}
@@ -34,8 +44,12 @@ describe('loginController', function() {
         $httpBackend.flush();
         resp = tokenResponse();
         expect(scope.token).toEqual(resp.token);
+        expect(scope.tokenClaims).toEqual(tokenClaims());
+    });
 
-
-
+    it('Logout function', function(){
+        scope.logout();
+        expect(scope.token).not.toBeDefined();
+        expect(scope.tokenClaims).not.toBeDefined();
     });
 });
